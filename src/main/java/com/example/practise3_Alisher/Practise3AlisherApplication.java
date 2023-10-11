bf90f274292cac05a84744b38bb3aae556481920
olesherka <smoke.paladin@gmail.com>
2023-10-11 19:47:17 +0600

    Add existing project files to Git

---
+++ src/main/java/com/example/practise3_Alisher/Practise3AlisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.practise3_Alisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practise3AlisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practise3AlisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practise3_Alisher/Practise3AlisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practise3_Alisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practise3AlisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
