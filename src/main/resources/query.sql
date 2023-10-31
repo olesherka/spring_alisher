749ac113f9b880cbc002169b95780df03c820355
olesherka <smoke.paladin@gmail.com>
2023-10-31 11:15:34 +0600

    practice5 commit

---
+++ src/main/resources/query.sql
@@ -0,0 +1,4 @@
+INSERT INTO sudents values (1, "alisher", "ablekimov");
+
+
+INSERT INTO teachers values (1, "nurbolat", "maratovich");
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE students(
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(20),
+    student_lname VARCHAR(20),
+)
+
+CREATE TABLE teachers(
+     teacher_id INT PRIMARY KEY,
+     teacher_name VARCHAR(20),
+     teacher_lname VARCHAR(20),
+)
+
+CREATE TABLE discipline(
+    discipline_id INT PRIMARY KEY,
+    discipline_name varchar(20),
+)
+
+CREATE TABLE events(
+    event_id   INT PRIMARY KEY,
+    event_name varchar(20),
+    event_date date,
+    teacher_id int,
+    student_id int
+)
+
+CREATE TABLE schedule_of_events
+(
+    schedule_id   INT PRIMARY KEY,
+    event_id      date,
+    teacher_id    int,
+    students_id   int,
+    discipline_id int
+)
